67c5b4081e57e5ea2e79f8399f7083bfb47da150
aCandidMind <opensource@candidminds.com>
2013-12-09 06:55:50 +0100

    As we'll consume an API that is JSON API compliant (http://jsonapi.org/), we have to get the proper key from the response.

--- app/scripts/collections/spots.js
+++ app/scripts/collections/spots.js
@@ -1,15 +1,21 @@
 define([
-	'backbone'
+	'backbone',
+	'models/spot'
 ],
-function( Backbone ) {
+function( Backbone, Spot ) {
     'use strict';
 
 	/* Return a collection class definition */
 	return Backbone.Collection.extend({
+		model: Spot,
 		url: "http://localhost:3000/spots",
 
 		initialize: function() {
 			console.log("initialize a Spots collection");
+		},
+
+		parse: function(response) {
+			return response.spots;
 		}
 	});
 });
