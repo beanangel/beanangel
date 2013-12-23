6f531ae053331df5d7dc81bf48795aff1e1e4596
aCandidMind <opensource@candidminds.com>
2013-12-23 08:46:01 +0100

    Communicator needs to append a port to the backendURL also if the request is to be sent inside the local network, not only when it's on your own development device. For example to check the page on the phone.
    Of course this addition of the port to the backendURL can be omitted
    when configuring a host on your machine and thus enabling the site to
    be available at an address without an explicit port, but a lot of people
    simply won't do this.

--- app/scripts/communicator.js
+++ app/scripts/communicator.js
@@ -18,13 +18,8 @@ function( Backbone ) {
 			// create commands
 			this.command = new Backbone.Wreqr.Commands();
 
-			// TODO re-evaluate whether this is a good place, maybe we'll have a config on the application or something
-			// like that. But there where articles recommending against a centralized app instance that get's required
-			// too often.
-			this.backendURL = "http://" + location.hostname;
-			if(location.hostname === "localhost") {
-				this.backendURL += ":3000";
-			}
+			// see TODO on the function itself
+			this.initBackendURL();
 
 			this.mediator.on('all', this.logMediatorEvents, this);
 		},
@@ -34,6 +29,19 @@ function( Backbone ) {
 				console.log(eventName);
 			} else {
 				console.log(eventName, options);
+			}
+		},
+
+		// TODO re-evaluate whether this is a good place, maybe we'll have a config on the application or something
+		// like that. But there where articles recommending against a centralized app instance that get's required
+		// too often.
+		initBackendURL: function() {
+			this.backendURL = "http://" + location.hostname;
+			var isLocalNetwork = location.hostname === "localhost" ||
+								 location.hostname.substr(0, 8) == "192.168." ||
+								 location.hostname.substr(0, 5) == "10.0.";
+			if(isLocalNetwork) {
+				this.backendURL += ":3000";
 			}
 		}
 	});
